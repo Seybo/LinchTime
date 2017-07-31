b0ff4a756d08ce5d9cd13d676180da9f972fd322
glaux <mirmikh@gmail.com>
2017-07-31 09:31:24 +0300

    Add Meal model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731060449) do
+ActiveRecord::Schema.define(version: 20170731062758) do
+
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"
