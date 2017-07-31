41a9edac54c57e6fd6edd18db8f0df3e9965fbab
glaux <mirmikh@gmail.com>
2017-07-31 09:23:54 +0300

    Add Person model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+case Rails.env
+when "development"
+  Person.create(name: 'Enot')
+  Person.create(name: 'Fenec')
+  Person.create(name: 'Tom')
+  Person.create(name: 'Simba')
+end
